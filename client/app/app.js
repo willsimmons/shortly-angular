f7874d61e28a45eaa52e768d00bf3741580ab69b
William Simmons <willsimmons@gmail.com>
2016-10-14 11:55:23 -0700

    added remaining routes

--- client/app/app.js
+++ client/app/app.js
@@ -15,6 +15,14 @@ angular.module('shortly', [
       templateUrl: 'app/auth/signup.html',
       controller: 'AuthController'
     })
+    .when('/links', {
+      templateUrl: 'app/links/links.html',
+      controller: 'LinksController'
+    })
+    .when('/shorten', {
+      templateUrl: 'ap/shorten/shorten.html',
+      controller: 'LinksController'
+    });
     // Your code here
 
     // We add our $httpInterceptor into the array
