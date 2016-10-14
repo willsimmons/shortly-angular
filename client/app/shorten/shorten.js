46c11d8d4b9160246273a449e8b0bdfc70f71623
William Simmons <willsimmons@gmail.com>
2016-10-14 11:43:33 -0700

    add link form working on shorten

--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -1,5 +1,15 @@
 angular.module('shortly.shorten', [])
 
 .controller('ShortenController', function ($scope, $location, Links) {
-  // Your code here
+  $scope.link = {};
+
+  $scope.shorten = function() {
+    Links.create($scope.link)
+      .then(function () {
+        $location.path('/links');
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  };// Your code here
 });
